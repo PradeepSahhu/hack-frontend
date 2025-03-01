a1d72a607a0867eca31f816898237ee2aeb391e0
Pradeep Sahu <official.pradeepsahu@gmail.com>
2025-03-02 01:41:29 +0530

    completed

--- src/components/pages/Ten_days.jsx
+++ src/components/pages/Ten_days.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Windgraph from "../windgraph";
-import Solargraph from "../solargraph";
+import Windgraph from "../Windgraph.jsx";
+import Solargraph from "../solargraph.jsx";
 const Ten_days = () => {
   return (
     <>
