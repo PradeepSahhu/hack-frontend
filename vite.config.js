9bb86f36cb0fc752dc292d303cf0c4deb5ec7205
Pradeep Sahu <official.pradeepsahu@gmail.com>
2025-03-02 01:27:22 +0530

    completed

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  build:{
-    outDir:"dist",
-    emptyOutDir: true,
-  }
-})
+  plugins: [react(), tailwindcss()],
+});
