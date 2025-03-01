5d4e1b7bec32288e8b164992b4ba33befc20e698
Pradeep Sahu <official.pradeepsahu@gmail.com>
2025-03-02 01:40:47 +0530

    completed

--- src/components/pages/Ten_days.jsx
+++ src/components/pages/Ten_days.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import Windgraph from '../windgraph'
-import Solargraph from '../solargraph'
+import React from "react";
+import Windgraph from "../windgraph";
+import Solargraph from "../solargraph";
 const Ten_days = () => {
   return (
     <>
-        <div style={{display:'flex', justifyContent:'center', alignItems:'center',flexDirection:'column'}}>
-            <Windgraph />
-            <Solargraph />
-        </div>
-
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Windgraph />
+        <Solargraph />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Ten_days
+export default Ten_days;
